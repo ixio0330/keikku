316ab031102a61bae7bea53000bb35d117ada90b
ixio0330 <ixio0330@gmail.com>
2024-01-03 17:29:12 +0900

    :fire: Swagger UI 제거

--- app/swagger/page.tsx
+++
@@ -1,7 +0,0 @@
-import setupSwagger from '@/swagger';
-import SwaggerUI from './ui';
-
-export default async function SwaggerPage() {
-  const spec = await setupSwagger();
-  return <SwaggerUI spec={spec} />;
-}
