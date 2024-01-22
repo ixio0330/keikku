cb13e479cda42312ca3e77f2afe57d75c38f9fac
ixio0330 <ixio0330@gmail.com>
2024-01-22 21:59:54 +0900

    ✏️ 환경 변수명 변경

--- supabase.ts
+++ supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 import { Database } from "./types/supabase";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? '';
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '';
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON ?? '';
 const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 
 export default supabase;
