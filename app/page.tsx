7cef57c351dbee0327379919167eb8f111d9c6f3
ixio0330 <ixio0330@gmail.com>
2024-04-15 14:04:55 +0900

    👔 SNS 로그인 연동

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,26 @@
 'use client'
 
-import { useEffect } from "react"
-import useAuth from "./(hooks)/useAuth"
+import { useEffect } from 'react'
+import useAuth from './(hooks)/useAuth'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
   const { initAuth } = useAuth();
+  const router = useRouter();
+
   useEffect(() => {
     initAuth();
   }, []);
 
+  const redirectToCreateCakePage = () => {
+    router.push('/create-cake');
+  };
+
   return (
-    <h1>Home</h1>
+    <>
+      <h1>Home</h1>
+      {/* TODO 본인 체크 */}
+      <button onClick={redirectToCreateCakePage}>케이크 만들러 가기</button>
+    </>
   )
 }
