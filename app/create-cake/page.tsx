2b908ae8c4dad85c535f9b053bde7ce7a520c88d
ixio0330 <ixio0330@gmail.com>
2024-04-17 19:49:22 +0900

    🚧 케이크 제작 페이지 탭 구현

--- app/create-cake/page.tsx
+++ app/create-cake/page.tsx
@@ -1,9 +1,77 @@
 'use client'
 
+import { BsChevronLeft } from 'react-icons/bs'
+import { useState } from 'react'
+
+type TabList = 'shape' | 'text' | 'outline' | 'deco'
+const tabList: { id: TabList; text: string }[] = [
+  {
+    id: 'shape',
+    text: '모양'
+  },
+  {
+    id: 'text',
+    text: '텍스트'
+  },
+  {
+    id: 'outline',
+    text: '테두리'
+  },
+  {
+    id: 'deco',
+    text: '데코'
+  },
+];
+
+
 export default function CreateCake() {
+  const [currentTab, setCurrentTab] = useState<TabList>('shape');
+  const updateCurrentTab = (tab: TabList) => {
+    setCurrentTab(tab);
+  };
+  const getTabTextColor = (result: boolean) => result ? 'text-[#175428] border-[#175428]' : 'text-gray-400 border-transparent';
+
+  const [shape, setShape] = useState('');
+  const [text, setText] = useState('');
+  const [outline, setOutline] = useState('');
+  const [deco, setDeco] = useState('');
+
+  const getTabContent = () => {
+    switch (currentTab) {
+      case 'shape':
+        return <div>모양 선택 컴포넌트</div>
+      case 'text':
+        return <div>텍스트 선택 컴포넌트</div>
+      case 'outline':
+        return <div>테두리 선택 컴포넌트</div>
+      case 'deco':
+        return <div>데코 선택 컴포넌트</div>
+    }
+  };
+
   return (
-    <>
-      <h1>케이크 만들기</h1>
-    </>
+    <div className='w-96 px-3 m-auto border-box'>
+      <header className='py-3.5 flex justify-between items-center'>
+        <BsChevronLeft />
+        <h1 className='text-lg font-bold'>케이크 제작</h1>
+        <span></span>
+      </header>
+      <section>
+        <ul className='py-3.5 flex justify-between items-center text-lg font-bold text-gray-400'>
+          {
+            tabList.map(({ id, text }) => (
+              <li 
+                className={`${getTabTextColor(currentTab === id)} w-full text-center border-b-2`}
+                key={id}
+                onClick={() => updateCurrentTab(id)}
+              >
+                {text}
+              </li>
+            ))
+          }
+        </ul>
+        {getTabContent()}
+      </section>
+    </div>
   )
 }
