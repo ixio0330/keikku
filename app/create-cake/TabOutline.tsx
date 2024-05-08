961d2d8b37b8845aec2b868be1a16d5518ac5752
ixio0330 <ixio0330@gmail.com>
2024-05-08 20:17:30 +0900

    🚧 레이아웃 구성

--- app/create-cake/TabOutline.tsx
+++ app/create-cake/TabOutline.tsx
@@ -1,18 +1,23 @@
 export default function TabText({ onClickPrev, onClickNext }: { onClickPrev: () => void, onClickNext: () => void }) {
   return (
-    <div className="flex w-full absolute bottom-0 left-0 py-4">
-      <button
-        onClick={onClickPrev} 
-        className='w-3/5 text-[#175428] font-semibold border border-[#175428] rounded-lg box-border p-2 bg-white font-xs mr-4'
-      >
-        이전
-      </button>
-      <button 
-        onClick={onClickNext}
-        className='w-full text-white font-semibold border border-[#175428] rounded-lg box-border p-2 bg-[#175428] font-xs'
-      >
-        다음
-      </button>
+    <div className="flex flex-col space-y-10">
+      <div className="m-auto border rounded-2xl w-80 h-80 bg-[#FFF8EB] flex justify-center items-center">
+        
+      </div>
+      <div className="flex w-full absolute bottom-0 left-0 py-4">
+        <button
+          onClick={onClickPrev} 
+          className='w-3/5 text-[#175428] font-semibold border border-[#175428] rounded-lg box-border p-2 bg-white font-xs mr-4'
+        >
+          이전
+        </button>
+        <button 
+          onClick={onClickNext}
+          className='w-full text-white font-semibold border border-[#175428] rounded-lg box-border p-2 bg-[#175428] font-xs'
+        >
+          다음
+        </button>
+      </div>
     </div>
   )
 }
