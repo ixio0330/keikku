ff0b451af12a69a2b99b43aba4794f728eb852fc
ixio0330 <ixio0330@gmail.com>
2024-09-14 08:17:34 +0900

    🔥 템플릿 제거

---
+++ src/app/layout.jsx
@@ -0,0 +1,26 @@
+import localFont from "next/font/local";
+import "./globals.css";
+
+const pretendard = localFont({
+  src: "./fonts/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+  variable: "--font-pretendard",
+})
+
+export const metadata = {
+  title: "케이꾸",
+  description: "케이꾸",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ko">
+      <body
+        className={`${pretendard.variable}`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
