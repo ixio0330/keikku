32cb003347c60e457da2d7d3570e9eede93f7059
ixio0330 <ixio0330@gmail.com>
2024-09-14 08:11:43 +0900

    🔧 supabase 세팅

---
+++ src/supabase/index.js
@@ -0,0 +1,25 @@
+import { createServerClient } from "@supabase/ssr"
+import { cookies } from "next/headers"
+
+export default function createSupabase() {
+  const cookieStore = cookies()
+
+  return createServerClient(
+    `https://${process.env.SUPABASE_DOMAIN}`,
+    process.env.SUPABASE_ANON_KEY,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll()
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options),
+            )
+          } catch {}
+        },
+      },
+    },
+  )
+}
