839971a21c8a538ddb9cfd6c983123820463c3a0
ixio0330 <ixio0330@gmail.com>
2024-10-15 09:33:30 +0900

    🔍 메타 태그

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -9,9 +9,35 @@ const pretendard = localFont({
   variable: "--font-pretendard",
 })
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  minimumScale: 1,
+}
+
+const metadataBase = new URL(process.env.DOMAIN)
+
 export const metadata = {
   title: "케이꾸",
-  description: "케이꾸",
+  description: "일상을 특별하게, 케이꾸",
+  author: "케이꾸팀",
+  openGraph: {
+    title: "케이꾸",
+    site_name: "케이꾸",
+    url: metadataBase,
+    description: "일상을 특별하게, 케이꾸",
+    type: "website",
+    images: [
+      {
+        url: "/opengraph-image.png",
+        width: 1200,
+        height: 630,
+        alt: "케이꾸",
+      },
+    ],
+  },
 }
 
 export default function RootLayout({ children }) {
