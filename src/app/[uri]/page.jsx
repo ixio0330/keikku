4c52d55f70491ede23b13b2661e5505e8ab00da8
ixio0330 <ixio0330@gmail.com>
2024-11-03 11:44:48 +0900

    💄 배경 사진 제거

--- src/app/[uri]/page.jsx
+++ src/app/[uri]/page.jsx
@@ -1,4 +1,3 @@
-import Image from "next/image"
 import Link from "next/link"
 
 // db
@@ -31,61 +30,41 @@ export default async function KeikkuPage({ params }) {
     <div className="bg-background">
       <AppHeader userInfo={userInfo} />
       <main className="m-auto mt-16 min-h-[calc(100vh-64px)] max-w-content bg-background pb-10">
-        <section className="relative overflow-hidden px-content pb-28 pt-5">
-          <div className="absolute z-20">
-            {activeEvent ? (
-              <div className="space-y">
-                <p className="text-sm">
-                  {activeEvent.username}님이 진행중인 이벤트에요!
-                </p>
-                <p className="text-xl font-bold">{activeEvent.name}</p>
-                <div className="flex items-center gap-2 text-sm text-gray-400">
-                  <IoMdCalendar size={20} />
-                  <p>{activeEvent.date}</p>
-                  {activeEvent.category && (
-                    <>
-                      <div className="h-4 w-px bg-gray-400" />
-                      <p>{activeEvent.category}</p>
-                    </>
-                  )}
-                </div>
-                {isOwner && (
-                  <Link
-                    href={{
-                      pathname: "/event/update",
-                      query: { id: activeEvent.id },
-                    }}
-                    className="inline-block rounded-full bg-primary px-2 py-px text-white"
-                  >
-                    수정하기
-                  </Link>
+        <section className="relative overflow-hidden px-content">
+          {activeEvent ? (
+            <div className="space-y">
+              <p className="text-sm">
+                {activeEvent.username}님이 진행중인 이벤트에요!
+              </p>
+              <p className="text-xl font-bold">{activeEvent.name}</p>
+              <div className="flex items-center gap-2 text-sm text-gray-400">
+                <IoMdCalendar size={20} />
+                <p>{activeEvent.date}</p>
+                {activeEvent.category && (
+                  <>
+                    <div className="h-4 w-px bg-gray-400" />
+                    <p>{activeEvent.category}</p>
+                  </>
                 )}
               </div>
-            ) : (
-              <div className="text-xl">
-                <p>현재 진행 중인</p>
-                <p>이벤트가 없어요 ;(</p>
-              </div>
-            )}
-          </div>
-          <ul className="absolute -right-8 top-5 z-10 flex gap-2">
-            <li>
-              <Image
-                width={80}
-                height={80}
-                alt="이미지1"
-                src="/main/img01.png"
-              />
-            </li>
-            <li>
-              <Image
-                width={80}
-                height={80}
-                alt="이미지2"
-                src="/main/img02.png"
-              />
-            </li>
-          </ul>
+              {isOwner && (
+                <Link
+                  href={{
+                    pathname: "/event/update",
+                    query: { id: activeEvent.id },
+                  }}
+                  className="inline-block rounded-full bg-primary px-2 py-px text-white"
+                >
+                  수정하기
+                </Link>
+              )}
+            </div>
+          ) : (
+            <div className="text-xl">
+              <p>현재 진행 중인</p>
+              <p>이벤트가 없어요 ;(</p>
+            </div>
+          )}
         </section>
 
         {/* 쇼케이스 */}
